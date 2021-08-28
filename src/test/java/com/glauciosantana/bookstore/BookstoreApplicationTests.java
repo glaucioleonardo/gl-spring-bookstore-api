60a3f55b38f578a4fd2654c13320cf12ad3cd731
glaucioleonardo <glaucioleonardo@hotmail.com>
2021-08-28 17:25:11 -0300

    Initial commit

---
+++ src/test/java/com/glauciosantana/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glauciosantana.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
