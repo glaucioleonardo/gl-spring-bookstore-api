5e69d86a3ad9b6623f8babd9a704bbe063bfa241
glaucioleonardo <glaucioleonardo@hotmail.com>
2021-08-29 00:05:38 -0300

    Put and patch for book

--- src/main/java/com/glauciosantana/bookstore/resources/LivroResource.java
+++ src/main/java/com/glauciosantana/bookstore/resources/LivroResource.java
@@ -29,4 +29,16 @@ public class LivroResource {
         List<LivroDTO> listDTO = list.stream().map(obj -> new LivroDTO(obj)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDTO);
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Livro> update(@PathVariable Integer id, @RequestBody Livro obj) {
+        Livro newObj = service.update(id, obj);
+        return ResponseEntity.ok().body(newObj);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Livro> updatePatch(@PathVariable Integer id, @RequestBody Livro obj) {
+        Livro newObj = service.update(id, obj);
+        return ResponseEntity.ok().body(newObj);
+    }
 }
