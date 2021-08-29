f5d0b22b67d385ad0b84c6ca40790653dc182de8
glaucioleonardo <glaucioleonardo@hotmail.com>
2021-08-29 00:17:40 -0300

    Post for book

--- src/main/java/com/glauciosantana/bookstore/services/LivroService.java
+++ src/main/java/com/glauciosantana/bookstore/services/LivroService.java
@@ -1,5 +1,6 @@
 package com.glauciosantana.bookstore.services;
 
+import com.glauciosantana.bookstore.domain.Categoria;
 import com.glauciosantana.bookstore.domain.Livro;
 import com.glauciosantana.bookstore.repositories.LivroRepository;
 import com.glauciosantana.bookstore.services.exceptions.ObjectNotFoundException;
@@ -38,5 +39,12 @@ public class LivroService {
         newObj.setTitulo(obj.getTitulo());
         newObj.setNome_autor(obj.getNome_autor());
         newObj.setTexto(obj.getTexto());
+    }
+
+    public Livro create(Integer id_cat, Livro obj) {
+        obj.setId(null);
+        Categoria cat = categoriaService.findById(id_cat);
+        obj.setCategoria(cat);
+        return repository.save(obj);
     }
 }
